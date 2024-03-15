4de9ac19f55b9acbe256822320228969ae0ed669
Rahul <rahuldevan2019@gmail.com>
2024-03-15 13:49:06 +0530

    having issue in db link

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
 
-const connectToDB = mongoose
-    .connect(``)
-    .then(()=>{
-        console.log('connect to DB successfully...');
-    })
-    .catch((error)=>console.log("Error in connecting DB",error))
+const connectToDB = async () =>
+    await mongoose.connect(process.env.MONGO_URL)
+        .then(() => {
+            console.log('connect to DB successfully...');
+        })
+        .catch((error) => console.log("Error in connecting DB", error))
 
-module.exports=connectToDB;
+module.exports = connectToDB;
